e1168b36a831413e552d72cabcbf568e55633bc0
Sagar Karandikar <karandikarsagar@gmail.com>
2013-12-24 22:24:32 -0500

    cleanup the compileserver

--- compileserver/server.js
+++ compileserver/server.js
@@ -1,95 +1,39 @@
-var http = require('http');
-//var app = require('app');
+// This is the riscv-gcc compileserver for ANGEL's Write C functionality
+// It takes the posted content and compiles it using riscv-gcc
+// [todo] - add support for varying filenames to support multiple users
+// aka don't use a.out
 
+var http = require('http');
 
 http.createServer(function (req, res) {
     switch(req.url) {
         case '/':
             if (req.method == 'POST') {
-                console.log("POSTED");
                 recstr = "";
                 req.on('data', function(chunk) {
                     recstr += chunk.toString();
                 });
                 req.on('end', function() {
                     res.writeHead(200, "OK", {'Content-Type': 'text/plain', 'charset': 'x-user-defined', 'Access-Control-Allow-Origin': '*'});
-//                    res.end();
-                    console.log(recstr);
-
 
                     var sys = require('util')
                     var exec = require('child_process').exec
                     var child;
 
-
-
+                    // run riscv-gcc on the user C code
                     child = exec("echo " + recstr + " | riscv-gcc -xc -" , function (error, stdout, stderr) {
-                        sys.print('stdout: ' + stdout + "\n");
-                        sys.print('stderr: ' + stderr + "\n");
-                        if (error !== null) {
-                            console.log('exec error: ' + error);
-                        }
-//                        res.write(stdout);
-//                        res.end();
-
-                        /// TEST WRITING TO FILE
-                        var fs = require('fs');                       
-
-                        fs.readFile('a.out', function (err, data) {
-                            if(err) throw err;
-                            console.log(data);
-                            res.write(data);
-                            res.end();
-
-                fs.writeFile("a.out2", data, function(err) {
-                    if(err) {
-                        console.log(err);
-                    } else {
-                        console.log("The file was saved!");
-                    }
-                }); 
-
-
-
-
-
-                        });
-
-
-                        /// END TEST WRITING TO FILE
-
+                        // don't console.log / output anything
+                        //sys.print('stdout: ' + stdout + "\n");
+                        //sys.print('stderr: ' + stderr + "\n");
+                        //if (error !== null) {
+                        //    console.log('exec error: ' + error);
+                        //}
                     });
 
-
-
-
-                    setTimeout(runner, 3000);
                 });
             }
             break;
     }
 }).listen('40000', '0.0.0.0');
 
-console.log("server running at: 40000");
-
-
-function runner() {
-var sys = require('util')
-var exec = require('child_process').exec
-var child;
-
-child = exec("./a.out" , function (error, stdout, stderr) {
-    sys.print('stdout: ' + stdout + "\n");
-    sys.print('stderr: ' + stderr + "\n");
-/*    if (error !== null) {
-        console.log('\n exec error: ' + error);
-    }*/
-
-
-
-
-});
-
-
-}
-
+console.log("riscv-gcc compileserver running at: localhost:40000");
