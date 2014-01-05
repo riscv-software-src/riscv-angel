b299bd74bb023cd8430932190f0f26b7b05e13bf
Sagar Karandikar <karandikarsagar@gmail.com>
2014-01-05 16:52:24 -0500

    update instruction objects to align with new spec. also more performant inst object instantiation

--- inst.js
+++ inst.js
@@ -2,83 +2,52 @@
 
 function instruction(instVal) {
     this.inst = instVal;
-
-    function get_opcode() {
-        return (this.inst & 0x0000007F);
-    }
-
-    function get_rd() {
-        return (this.inst >>> 27);
-    }
-
-    function get_rs1() {
-        return ((this.inst >>> 22) & 0x0000001F);
-    }
-
-    function get_rs2() {
-        return ((this.inst >>> 17) & 0x0000001F);
-    }
-
-    function get_rs3() {
-        return ((this.inst >>> 12) & 0x0000001F);
-    }
-
-    function get_funct3() {
-        return ((this.inst >>> 7) & 0x00000007);
-    }
-
-    function get_funct5() {
-        return ((this.inst >>> 7) & 0x0000001F);
-    }
-
-    function get_funct10() {
-        return ((this.inst >>> 7) & 0x000003FF);
-    }
-
-    // inst type should be I or B
-    function get_imm11_7(inst_type) {
-        if (inst_type === 'I') {
-            return ((this.inst >>> 17) & 0x0000001F);
-        } else if (inst_type === 'B') {
-            return (this.inst >>> 27);
-        }
-    }
-
-    function get_imm6_0() {
-        return ((this.inst >>> 10) & 0x0000007F);
-    }
-
-    // inst type should be I or B
-    function get_imm(inst_type) {
-        if (inst_type === undefined) {
-            throw new RISCVError("ERR NO TYPE PROVIDED FOR IMMEDIATE FETCH");
-        }
-        return ((this.get_imm11_7(inst_type) << 7) | this.get_imm6_0());
-    }
-
-    function get_lui_imm() {
-        return ((this.inst >>> 7) & 0x000FFFFF);
-    }
-
-    function get_jump_offset() {
-        return ((this.inst >>> 7) & 0x01FFFFFF);
-    }
-
-    // tack on the methods
-    this.get_opcode = get_opcode;
-    this.get_rd = get_rd;
-    this.get_rs1 = get_rs1;
-    this.get_rs2 = get_rs2;
-    this.get_rs3 = get_rs3;
-    this.get_funct3 = get_funct3;
-    this.get_funct5 = get_funct5;
-    this.get_funct10 = get_funct10;
-    this.get_imm11_7 = get_imm11_7;
-    this.get_imm6_0 = get_imm6_0;
-    this.get_imm = get_imm;
-    this.get_lui_imm = get_lui_imm;
-    this.get_jump_offset = get_jump_offset;
 }
+
+/* Methods for instruction objects */
+
+instruction.prototype.get_opcode = function() {
+    return (this.inst & 0x0000007F);
+};
+
+instruction.prototype.get_rd = function() {
+    return ((this.inst >>> 7) & 0x0000001F);
+};
+
+instruction.prototype.get_rs1 = function() {
+    return ((this.inst >>> 15) & 0x0000001F);
+};
+
+instruction.prototype.get_rs2 = function() {
+    return ((this.inst >>> 20) & 0x0000001F);
+};
+
+instruction.prototype.get_funct3 = function() {
+    return ((this.inst >>> 12) & 0x00000007);
+};
+
+instruction.prototype.get_funct7 = function() {
+    return ((this.inst >>> 25) & 0x0000007F);
+};
+
+/* lower 12 bits of return value contain immediate */
+instruction.prototype.get_I_imm = function() { // NEW
+    return ((this.inst >>> 20) & 0x00000FFF);
+};
+
+/* lower 12 bits of return value contain immediate */
+instruction.prototype.get_S_imm = function() { // NEW
+    return (((this.inst >>> 25) & 0x0000007F) << 5) | ((this.inst >>> 7) & 0x0000001F);
+};
+
+/* lower 20 bits of return value contain immediate */
+instruction.prototype.get_U_imm = function() { // NEW
+    return ((this.inst >>> 12) & 0x000FFFFF);
+};
+
+/* End Methods for instruction objects */
+
+
 
 // "sign extend" the quantity based on bit
 // quantity will be a 32 bit quantity that was zero extended by default 
