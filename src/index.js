5d559843280533a4ced2298f6aa5def15502a863
Zorrow2008 <yokesh98@gmail.com>
2020-04-29 20:27:46 -0700

    passing lint, added package for types

--- src/index.js
+++ src/index.js
@@ -1,52 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import useCPU from './hooks/use-cpu';
 
 class Category extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor({ registers, name }) {
+    super({ registers, name });
     this.state = {
       showComponent: false,
     };
     this._onButtonClick = this._onButtonClick.bind(this);
-    this.name = props.name;
-    this.registers = props.registers
+    this.name = name;
+    this.registers = registers;
   }
 
   _onButtonClick() {
-    this.setState({
-      showComponent: !this.state.showComponent,
-    });
+    this.setState((prevState) => ({ showComponent: !prevState.showComponent }));
   }
 
   render() {
+    const { showComponent } = this.state;
     return (
       <div>
-        <button onClick={this._onButtonClick}>{this.name}</button>
-        {this.state.showComponent ?
-           <Registers registers={this.registers}/> :
-           null
-        }
+        <button type="submit" onClick={this._onButtonClick}>{this.name}</button>
+        {showComponent
+          ? <Registers registers={this.registers} />
+          : null}
       </div>
     );
   }
 }
 
-const RegisterPanel = (props) => {
-  const rs = props.registers.map((reg, i) => <li>{"x" + i + ": "+ reg.low_}</li>);
-  return <div>
-    <Category registers={rs.slice(0, 5)} name={"Pointers"}/>
-    <Category registers={rs.slice(5, 8).concat(rs.slice(28, 32))} name={"Temporary"}> Temporary</Category>
-    <Category registers={rs.slice(8, 10).concat(rs.slice(18, 28))} name={"Callee-saved"}> Callee-saved</Category>
-    <Category registers={rs.slice(10, 18)} name={"Arguments"}> Arguments </Category>
-  </div>
-}
+Category.propTypes = {
+  registers: PropTypes.arrayOf.isRequired,
+  name: PropTypes.string,
+};
 
-const Registers = (props) => {
-  return <div>
-    {props.registers}
+Category.defaultProps = {
+  name: undefined,
+};
+
+const RegisterPanel = ({ registers }) => {
+  const rs = registers.map((reg, i) => (
+    <li>
+      x
+      { i }
+      :
+      { reg.low_}
+    </li>
+  ));
+  return (
+    <div>
+      <Category registers={rs.slice(0, 5)} name="Pointers" />
+      <Category registers={rs.slice(5, 8).concat(rs.slice(28, 32))} name="Temporary" />
+      <Category registers={rs.slice(8, 10).concat(rs.slice(18, 28))} name="Callee-saved" />
+      <Category registers={rs.slice(10, 18)} name="Arguments" />
+    </div>
+  );
+};
+
+RegisterPanel.propTypes = {
+  registers: PropTypes.arrayOf.isRequired,
+};
+
+const Registers = ({ registers }) => (
+  <div>
+    {registers}
   </div>
-}
+);
+
+Registers.propTypes = {
+  registers: PropTypes.arrayOf.isRequired,
+};
 
 
 const App = () => {
@@ -58,19 +83,12 @@ const App = () => {
 
   console.log(cpu);
 
-  const rs = cpu.registers.map((reg, i) => <li>{"x" + i + ": "+ reg.low_}</li>);
-  console.log(rs);
-
   return (
     <>
       <h1>hello world</h1>
-      <RegisterPanel registers={cpu.registers}/>
-      {/* <RegisterMenu registers={rs}/>
-      <RegisterBoard registers={rs} /> */}
+      <RegisterPanel registers={cpu.registers} />
     </>
   );
 };
-
-//test
 
 ReactDOM.render(<App />, document.querySelector('#root'));
